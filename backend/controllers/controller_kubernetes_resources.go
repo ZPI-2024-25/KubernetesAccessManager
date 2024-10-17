abc2c321a1ece414fa9360e23710de0865dfc037
Samuel Zoladz <samuel.zoladz@gmail.com>
2024-10-17 19:17:04 +0200

    ZPI-35: Added /k8s/{resourceType} endpoints

--- backend/controllers/controller_kubernetes_resources.go
+++ backend/controllers/controller_kubernetes_resources.go
@@ -4,12 +4,32 @@ import (
 	"encoding/json"
 	"github.com/ZPI-2024-25/KubernetesAccessManager/kubernetes"
 	"github.com/gorilla/mux"
+	"io"
 	"net/http"
 )
 
 func CreateResourceController(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
+
+	// Read body
+	bodyVal, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode("Invalid request body")
+		return
+	}
+
+	resourceManifest := string(bodyVal)
+
+	resource, creationErr := kubernetes.ResourceCreation(resourceManifest)
+
+	if creationErr != nil {
+		w.WriteHeader(int(creationErr.Code))
+		json.NewEncoder(w).Encode(creationErr)
+	} else {
+		w.WriteHeader(201)
+		json.NewEncoder(w).Encode(resource)
+	}
 }
 
 func DeleteResourceController(w http.ResponseWriter, r *http.Request) {
@@ -24,14 +44,15 @@ func GetResourceController(w http.ResponseWriter, r *http.Request) {
 
 func ListResourcesController(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
 	params := mux.Vars(r)
-
 	resource, err := kubernetes.ResourceListing(params["resourceType"])
 	if err != nil {
-		http.Error(w, err.Message, int(err.Code))
+		w.WriteHeader(int(err.Code))
+		json.NewEncoder(w).Encode(err)
+	} else {
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(resource)
 	}
-	json.NewEncoder(w).Encode(resource)
 }
 
 func UpdateResourceController(w http.ResponseWriter, r *http.Request) {
--- backend/kubernetes/kubernetes_resources.go
+++ backend/kubernetes/kubernetes_resources.go
@@ -1,16 +1,39 @@
 package kubernetes
 
-import "github.com/ZPI-2024-25/KubernetesAccessManager/models"
+import (
+	"encoding/json"
+	"github.com/ZPI-2024-25/KubernetesAccessManager/models"
+)
 
 func ResourceListing(resourceType string) (models.ResourceList, *models.ModelError) {
+	if resourceType == "myszojelen" {
+		errorObj := models.ModelError{
+			Code:    400,
+			Message: "ZAGROZONY WYGINIECIEM",
+		}
+		return models.ResourceList{}, &errorObj
+	}
 	return models.ResourceList{
 		ResourceList: []models.ResourceListResourceList{
-			{Active: "aaa"},
-			{Active: "bbb"},
+			{Active: "MYSZO", Age: "2024-10-09T17:06:43Z"},
+			{Active: "JELEN", Age: "2005-04-02T19:37:00Z"},
 		},
 		Columns: []string{
-			"aaa",
-			"bbb",
+			"active",
+			"age",
 		},
 	}, nil
+}
+
+func ResourceCreation(resourceManifest string) (models.ResourceDetails, *models.ModelError) {
+	if resourceManifest == "myszojelen" {
+		errorObj := models.ModelError{
+			Code:    400,
+			Message: "ZAGROZONY WYGINIECIEM",
+		}
+		return models.ResourceDetails{}, &errorObj
+	}
+	var result interface{}
+	json.Unmarshal([]byte(resourceManifest), &result)
+	return models.ResourceDetails{ResourceDetails: &result}, nil
 }
