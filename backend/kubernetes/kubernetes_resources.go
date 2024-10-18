f349e572ba0523551d71ef91313e26a82519d69a
Samuel Zoladz <samuel.zoladz@gmail.com>
2024-10-18 16:32:56 +0200

    ZPI-35: Added all k8s endpoints

--- backend/kubernetes/kubernetes_resources.go
+++ backend/kubernetes/kubernetes_resources.go
@@ -37,3 +37,18 @@ func ResourceCreation(resourceManifest string) (models.ResourceDetails, *models.
 	json.Unmarshal([]byte(resourceManifest), &result)
 	return models.ResourceDetails{ResourceDetails: &result}, nil
 }
+
+func ResourceGet(resourceType string, resourceName string) (models.ResourceDetails, *models.ModelError) {
+	if resourceName == "myszojelen" || resourceType == "myszojelen" {
+		errorObj := models.ModelError{
+			Code:    400,
+			Message: "ZAGROZONY WYGINIECIEM",
+		}
+		return models.ResourceDetails{}, &errorObj
+	}
+	var resourceDetails interface{}
+	json.Unmarshal([]byte("{\n  \"apiVersion \":  \"v1 \",\n  \"kind \":  \"Pod \",\n  \"metadata \": {\n  \"name \":  \"example-pod \",\n  \"namespace \":  \"default \"\n },\n  \"spec \": {\n  \"containers \": [\n {\n  \"name \":  \"nginx \",\n  \"image \":  \"nginx:1.14.2 \"\n }\n ]\n },\n  \"status \": {\n  \"phase \":  \"Running \"\n }\n }\n"), &resourceDetails)
+	return models.ResourceDetails{
+		ResourceDetails: &resourceDetails,
+	}, nil
+}
