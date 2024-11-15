ef6f9c84fad46a0b236c7667d4f0aa33f8ef9dd0
Ninja00Shadow <mrshadow813@gmail.com>
2024-11-15 18:34:50 +0100

    Remove test 1=1, Remove .env

--- frontend/src/__tests__/App.test.tsx
+++ frontend/src/__tests__/App.test.tsx
@@ -4,10 +4,6 @@ import '@testing-library/jest-dom'
 import { render } from "@testing-library/react"
 import App from "../App.tsx";
 
-test('demo', () => {
-    expect(1).toBe(1);
-})
-
 test("Renders the main page", () => {
     render(<App />)
     expect(true).toBeTruthy()
