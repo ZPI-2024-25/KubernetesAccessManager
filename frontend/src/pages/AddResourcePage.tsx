314140c18d5f864b25882cb76637abb9e2028464
vgoriukhina <veraword44@gmail.com>
2024-11-18 16:48:36 +0300

    Fix imports

--- frontend/src/pages/AddResourcePage.tsx
+++ frontend/src/pages/AddResourcePage.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { InputForm } from '../components/AddForm/InputForm';  // Форма для добавления ресурса
 
 export const AddResourcePage: React.FC = () => {
     return (
         <div>
             <h2>Add Resource</h2>
-            <InputForm />
+            # TODO: Add component from EditorPage.tsx
         </div>
     );
 };
