a658ecb6e726fad1f897395c2402f88af83f5d1f
Ninja00Shadow <mrshadow813@gmail.com>
2024-11-18 21:30:15 +0100

    Clean Menu Items and Add Proper Icons

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,1 +1,7 @@
 export * from './k8s/listResources.ts'
+
+export * from './helm/listReleases.ts'
+export * from './helm/getRelease.ts'
+export * from './helm/uninstallRelease.ts'
+export * from './helm/getReleaseHistory.ts'
+export * from './helm/rollbackRelease.ts'
