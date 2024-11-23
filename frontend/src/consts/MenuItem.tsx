df656326c8353547a488db0617250496b0248e8d
Ninja00Shadow <mrshadow813@gmail.com>
2024-11-23 12:30:34 +0100

    Menu icons and only resources we can handle

--- frontend/src/consts/MenuItem.tsx
+++ frontend/src/consts/MenuItem.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { FileOutlined, PieChartOutlined, TeamOutlined, UserOutlined } from "@ant-design/icons";
-import { MenuItem } from "../types";
+import {SiHelm} from "react-icons/si";
+import {MdStorage, MdDashboardCustomize, MdAssuredWorkload, MdDriveFileRenameOutline} from "react-icons/md";
+import {FaCubes} from "react-icons/fa6";
+import {IoServerSharp, IoDocumentText, IoGitNetwork} from "react-icons/io5";
+
+import {MenuItem} from "../types";
 
 function getItem(
     label: React.ReactNode,
     key: React.Key,
-    resourcelabel:string,
+    resourceLabel: string,
     icon?: React.ReactNode,
     children?: MenuItem[],
 ): MenuItem {
@@ -14,67 +18,42 @@ function getItem(
         icon,
         children,
         label,
-        resourcelabel: resourcelabel
+        resourcelabel: resourceLabel
     } as MenuItem;
 }
 
 export const items: MenuItem[] = [
-    getItem('Cluster', '01', 'Cluster'),
-    getItem('Applications', '02', 'Applications'),
-    getItem('Nodes', '03', 'Node', <PieChartOutlined />),
-    getItem('Workloads', 'sub1', 'Workloads', <UserOutlined />, [
-        getItem('Overview', '04', 'Overview'),
-        getItem('Pods', '05', 'Pod'),
-        getItem('Deployments', '06', 'Deployment'),
-        getItem('Daemon Sets', '07', 'DaemonSet'),
-        getItem('Stateful Sets', '08', 'StatefulSet'),
-        getItem('Replica Sets', '09', 'ReplicaSet'),
-        getItem('Replication Controllers', '10', 'ReplicationController'),
-        getItem('Jobs', '11', 'Job'),
-        getItem('Cron Jobs', '12', 'CronJobs'),
+    getItem('Nodes', '01', 'Node', <IoServerSharp/>),
+    getItem('Workloads', 'sub1', 'Workloads', <FaCubes/>, [
+        getItem('Pods', '02', 'Pod'),
+        getItem('Deployments', '03', 'Deployment'),
+        getItem('Daemon Sets', '04', 'DaemonSet'),
+        getItem('Stateful Sets', '05', 'StatefulSet'),
+        getItem('Replica Sets', '06', 'ReplicaSet'),
+        getItem('Jobs', '07', 'Job'),
+        getItem('Cron Jobs', '08', 'CronJobs'),
     ]),
-    getItem('Config', 'sub2', 'Configs', <TeamOutlined />, [
-        getItem('Config Maps', '13', 'ConfigMap'),
-        getItem('Secrets', '14', 'Secret'),
-        getItem('Resource Quotas', '15', 'ResourceQuota'),
-        getItem('Limit Ranges', '16', 'LimitRange'),
-        getItem('Horizontal Pod Autoscalers', '17', 'HorizontalPodAutoscaler'),
-        getItem('Vertical Pod Autoscalers', '18', 'VerticalPodAutoscaler'),
-        getItem('Pod Disruption Budgets', '19', 'PodDisruptionBudget'),
-        getItem('Priority Classes', '20', 'PriorityClass'),
-        getItem('Runtime Classes', '21', 'RuntimeClass'),
-        getItem('Leases', '22', 'Leases'),
-        getItem('Mutating Webhook Configs', '23', 'MutatingWebhookConfig'),
-        getItem('Validating Webhook Configs', '24', 'ValidatingWebhookConfig'),
+    getItem('Config', 'sub2', 'Configs', <IoDocumentText/>, [
+        getItem('Config Maps', '09', 'ConfigMap'),
+        getItem('Secrets', '10', 'Secret'),
     ]),
-    getItem('Network', 'sub3', 'Network', <TeamOutlined />, [
-        getItem('Services', '25', 'Service'),
-        getItem('Endpoints', '26', 'Endpoint'),
-        getItem('Ingresses', '27', 'Ingress'),
-        getItem('Ingress Classes', '28', 'IngressClass'),
-        getItem('Network Policies', '29', 'NetworkPolicy'),
-        getItem('Port Forwarding', '30', 'PortForwarding'),
+    getItem('Network', 'sub3', 'Network', <IoGitNetwork/>, [
+        getItem('Services', '11', 'Service'),
+        getItem('Ingresses', '12', 'Ingress'),
     ]),
-    getItem('Storage', 'sub4', 'Storage', <TeamOutlined />, [
-        getItem('Persistent Volume Claims', '31', 'PersistentVolumeClaim'),
-        getItem('Persistent Volumes', '32', 'PersistentVolume'),
-        getItem('Storage Classes', '33', 'StorageClass'),
+    getItem('Storage', 'sub4', 'Storage', <MdStorage/>, [
+        getItem('Persistent Volume Claims', '13', 'PersistentVolumeClaim'),
+        getItem('Persistent Volumes', '14', 'PersistentVolume'),
+        getItem('Storage Classes', '15', 'StorageClass'),
     ]),
-    getItem('Namespaces', '34', 'Namespace', <FileOutlined />),
-    getItem('Events', '35', 'Event', <FileOutlined />),
-    getItem('Helm', 'sub5', 'Helm', <FileOutlined />, [
-        getItem('Charts', '36', 'Chart'),
-        getItem('Releases', '37', 'Release'),
+    getItem('Namespaces', '16', 'Namespace', <MdDriveFileRenameOutline/>),
+    getItem('Helm', '17', 'Helm', <SiHelm/>),
+    getItem('Access Control', 'sub6', 'AccessControl', <MdAssuredWorkload/>, [
+        getItem('Service Accounts', '18', 'ServiceAccount'),
+        getItem('Cluster Roles', '19', 'ClusterRole'),
+        getItem('Cluster Role Bindings', '20', 'ClusterRoleBinding'),
     ]),
-    getItem('Access Control', 'sub6', 'AccessControl', <TeamOutlined />, [
-        getItem('Service Accounts', '38', 'ServiceAccount'),
-        getItem('Cluster Roles', '39', 'ClusterRole'),
-        getItem('Roles', '40', 'Role'),
-        getItem('Cluster Role Bindings', '41', 'ClusterRoleBinding'),
-        getItem('Role Bindings', '42', 'RoleBinding'),
-        getItem('Pod Security Policies', '43', 'PodSecurityPolicy'),
-    ]),
-    getItem('Custom Resources', 'sub7', 'CustomResource', <TeamOutlined />, [
-        getItem('Definitions', '44', 'CustomResourceDefinition')
+    getItem('Custom Resources', 'sub7', 'CustomResource', <MdDashboardCustomize/>, [
+        getItem('Definitions', '21', 'CustomResourceDefinition')
     ]),
 ];
