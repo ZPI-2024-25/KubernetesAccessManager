9cb9f6485e9bb695e0ae2664c7d618882d88c8a0
samuelus <266592@student.pwr.edu.pl>
2024-11-26 16:18:55 +0100

    ZPI-xx: Update env on frontend(#41)
    Co-authored-by: Samuel Zoladz <samuel.zoladz@gmail.com>

--- frontend/src/consts/consts.ts
+++ frontend/src/consts/consts.ts
@@ -1,6 +1,6 @@
 export const API_PREFIX = import.meta.env.VITE_API_URL || 'http://localhost:8080/'
 export const API_URL = `${API_PREFIX}api/v1/k8s`;
-export const KEYCLOAK_URL = import.meta.env.KEYCLOAK_URL || 'http://localhost:4000/'
+export const KEYCLOAK_URL = import.meta.env.VITE_KEYCLOAK_URL || 'http://localhost:4000/'
 export const KEYCLOAK_LOGIN_URL = import.meta.env.KEYCLOAK_LOGIN_URL || `${KEYCLOAK_URL}realms/ZPI-realm/protocol/openid-connect/auth?client_id=ZPI-client&response_type=code&scope=openid`
 export const KEYCLOAK_LOGOUT_URL = import.meta.env.KEYCLOAK_LOGOUT_URL || `${KEYCLOAK_URL}realms/ZPI-realm/protocol/openid-connect/logout`
 export const KEYCLOAK_TOKEN_URL = import.meta.env.KEYCLOAK_TOKEN_URL || `${KEYCLOAK_URL}realms/ZPI-realm/protocol/openid-connect/token`
