4ededab9bdf7ad962d873c039fe6bbf8a9a59a8d
Ninja00Shadow <mrshadow813@gmail.com>
2024-12-01 13:27:56 +0100

    Fix wrong label for CronJob

--- frontend/src/consts/MenuItem.tsx
+++ frontend/src/consts/MenuItem.tsx
@@ -31,7 +31,7 @@ export const items: MenuItem[] = [
         getItem('Stateful Sets', '05', 'StatefulSet'),
         getItem('Replica Sets', '06', 'ReplicaSet'),
         getItem('Jobs', '07', 'Job'),
-        getItem('Cron Jobs', '08', 'CronJobs'),
+        getItem('Cron Jobs', '08', 'CronJob'),
     ]),
     getItem('Config', 'sub2', 'Configs', <IoDocumentText/>, [
         getItem('Config Maps', '09', 'ConfigMap'),
